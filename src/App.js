55b44c2994110675421209009fa80c6dabcc885b
anurag negi <anuragnegi008@gmail.com>
2021-03-31 20:07:52 +0530

    created product component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 //feature 1
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        React Products
-      </main>
-      <footer>
-        All Rights Reserved
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}  />
+            </div>
+            <div className="sidebar">
+              cart items
+            </div>
+          </div>
+        </main>
+        <footer>All Rights Reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
