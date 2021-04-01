5e85a53ceae391e6e7fd9c65c44fb924411500ce
anurag negi <anuragnegi008@gmail.com>
2021-04-01 22:00:09 +0530

    redux add product

--- src/App.js
+++ src/App.js
@@ -1,100 +1,124 @@
 //feature 1
 import React from "react";
+import { Provider } from "react-redux";
 import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json";
+import store from "./store";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
-      cartItems:JSON.parse(localStorage.getItem("cartItems"))?JSON.parse(localStorage.getItem("cartItems")):[],
+      cartItems: JSON.parse(localStorage.getItem("cartItems"))
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : [],
       size: "",
       sort: "",
     };
   }
 
-  createOrder=(order)=>{
-    alert("need to save order for"+order.name)
-  }
+  createOrder = (order) => {
+    alert("need to save order for" + order.name);
+  };
 
-  removeFromCart=(product)=>{
-    const cartItems=this.state.cartItems.slice()
-    this.setState({cartItems:cartItems.filter(x=>x._id!==product._id),});
-    localStorage.setItem("cartItems",JSON.stringify(cartItems.filter(x=>x._id!==product._id)))
-  }
-  addToCart=(product)=>{
-    const cartItems=this.state.cartItems.slice()
-    let alreadyInCart=false;
-    cartItems.forEach((items)=>{
-      if(items._id===product._id){
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );
+  };
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((items) => {
+      if (items._id === product._id) {
         items.count++;
-        alreadyInCart=true;
+        alreadyInCart = true;
       }
-    })
-    if(!alreadyInCart){
-        cartItems.push({...product,count:1});
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
     }
-    this.setState({cartItems})
-    localStorage.setItem("cartItems",JSON.stringify(cartItems))
-  }
-  sortProducts=(event)=>{
-    const sort=event.target.value;
-    this.setState((state)=>({
-      sort:sort,
-      products:this.state.products.slice().sort((a,b)=>(
-        sort==="lowest"?
-        ((a.price>b.price)?1:-1):
-        sort==="highest"?
-        ((a.price<b.price)?1:-1):
-        ((a._id>b._id?1:-1))
-      ))
-    }))
-  }
+    this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  };
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
 
-  filterProducts=(event)=>{
-    console.log(event.target.value)
-    if((event.target.value)===""){
-      this.setState({size:event.target.value,products:data.products});
-    }
-    else{
+  filterProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    } else {
       this.setState({
-        size:event.target.value,
-        products:data.products.filter(product=>product.availableSizes.indexOf(event.target.value)>=0)
-      })
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
     }
-    
-  }
+  };
 
   render() {
     return (
-      <div className="grid-container">
-        <header>
-          <a href="/">React Shopping Cart</a>
-        </header>
-        <main>
-          <div className="content">
-            <div className="main">
-              <Filter count={this.state.products.length} 
-              size={this.state.size}
-              sort={this.state.sort}
-              filterProducts={this.filterProducts}
-              sortProducts={this.sortProducts}
-              />
-              <Products products={this.state.products} addToCart={this.addToCart} />
+      <Provider store={store}>
+        <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                <Filter
+                  count={this.state.products.length}
+                  size={this.state.size}
+                  sort={this.state.sort}
+                  filterProducts={this.filterProducts}
+                  sortProducts={this.sortProducts}
+                />
+                <Products
+                  products={this.state.products}
+                  addToCart={this.addToCart}
+                />
+              </div>
+              <div className="sidebar">
+                <Cart
+                  cartItems={this.state.cartItems}
+                  removeFromCart={this.removeFromCart}
+                  createOrder={this.createOrder}
+                />
+              </div>
             </div>
-            <div className="sidebar">
-              <Cart cartItems={this.state.cartItems}
-              removeFromCart={this.removeFromCart}
-              createOrder={this.createOrder}
-              />
-            </div>
-          </div>
-        </main>
-        <footer>All Rights Reserved</footer>
-      </div>
+          </main>
+          <footer>All Rights Reserved</footer>
+        </div>
+      </Provider>
     );
   }
 }
