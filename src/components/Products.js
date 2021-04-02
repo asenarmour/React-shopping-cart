ae75113a83233238ae23ebbf82e8272257396044
anurag negi <anuragnegi008@gmail.com>
2021-04-02 09:22:58 +0530

    add cart to redux

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,7 +4,8 @@ import Fade from "react-reveal/Fade";
 import Modal from 'react-modal';
 import Zoom from 'react-reveal/Zoom';
 import { connect } from 'react-redux';
-import {fetchProducts} from '../actions/productActions'
+import {fetchProducts} from '../actions/productActions';
+import {addToCart} from '../actions/cartActions'
 
 class Products extends Component {
 
@@ -101,4 +102,4 @@ class Products extends Component {
     }
 }
 
-export default connect((state)=>({products:state.products.filteredItems}),{fetchProducts})(Products)
+export default connect((state)=>({products:state.products.filteredItems}),{fetchProducts,addToCart})(Products)
